a0dd08826d70a8bcf655e7f2650bd43ddd312a14
Takamasa Fukase <takama224@yahoo.co.jp>
2019-11-13 21:09:28 +0900

    Initial Commit

---
+++ AlamofireTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireTest
+//
+//  Created by 深瀬貴将 on 2019/11/13.
+//  Copyright © 2019 fukase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
